51d87a10ae4b54b353209464da9fcc4b22ece004
1014368 <87939358+1014368@users.noreply.github.com>
2021-09-22 17:49:10 +0530

    Add files via upload

---
+++ census_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import numpy as np 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import censusdata
+
+
+st.image(image='censusimg.jpg',caption='https://pixabay.com/illustrations/magnifying-glass-human-head-faces-1607208/')
+
+st.title('Census Data Exploration')
+st.header('Explore socially important metrics')
+st.write('This exploration uses data from American Community Survey 5-year data released in 2017.')
+st.write('More information about the ACS 5-year survey: https://www.census.gov/data/developers/data-sets/acs-5year.html')
+st.write("The data was collected using Julien Leider's CensusData pip package, which can be found at https://pypi.org/project/CensusData/ or installed with")
+st.header('County Level Summaries')
+st.write('This analysis will use data aggregated on the county level for the variables: Gini Index (income inequality index), Vacant Housing, Percent Unemployed and Median Family Income.')
+
+
+### SECTION 1: Querying and Cleaning Data ###
+
+
+     
+
+  
+
+    
+
+
+
+
+
+
+
+
+
